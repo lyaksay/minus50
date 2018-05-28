0eff3fde635bfbde67ee6ed4d2a822b00fee07d9
Maksim <vl-mail25@yandex.ru>
2018-05-28 18:51:52 +0700

    footer

--- html/partials/footer.php
+++ html/partials/footer.php
@@ -1,34 +1,60 @@
 <hr>
-<div class="container footer">
+<div class="container-fluid footer ">
     <div class="row">
-        <div class="col-md-8">
+        <div class="col-md-8 ">
             <div class="row">
                 <div class="col-md-3">
                     <div class="f__title">
                         Помощь
                     </div>
+                    <ul class="footer__list">
+                        <li><a href="">Условия оплаты</a></li>
+                        <li><a href="">Оплата</a></li>
+                        <li><a href="">Доставка</a></li>
+                        <li><a href="">Личные данные</a></li>
+                    </ul>
                 </div>
                 <div class="col-md-3">
                     <div class="f__title">
                         Профиль
                     </div>
+                    <ul class="footer__list">
+                        <li><a href="">Условия оплаты</a></li>
+                        <li><a href="">Оплата</a></li>
+                        <li><a href="">Доставка</a></li>
+                        <li><a href="">Личные данные</a></li>
+                    </ul>
                 </div>
                 <div class="col-md-3">
                     <div class="f__title">
                         О компании
                     </div>
+                    <ul class="footer__list">
+                        <li><a href="">Условия оплаты</a></li>
+                        <li><a href="">Оплата</a></li>
+                        <li><a href="">Доставка</a></li>
+                        <li><a href="">Личные данные</a></li>
+                    </ul>
                 </div>
                 <div class="col-md-3">
                     <div class="f__title">
                         Блог
                     </div>
+                    <ul class="footer__list">
+                        <li><a href="">Условия оплаты</a></li>
+                        <li><a href="">Оплата</a></li>
+                        <li><a href="">Доставка</a></li>
+                        <li><a href="">Личные данные</a></li>
+                    </ul>
                 </div>
             </div>
         </div>
-        <div class="col-md-4">
+
+        <div class="col-md-2 ml-auto">
             <div class="row">
                 <div class="col-md-12">
-                    Свяжитесь с нами
+                    Свяжитесь с нами <br> <br>
+                    8-913-383-32-21
                 </div>
             </div>
         </div>
